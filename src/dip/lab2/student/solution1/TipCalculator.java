aa6c5f7f1b5954b475a9ff45db48efe704f6b27c
dpaasch@my.wctc.edu <>
2013-02-07 20:38:41 -0600

    Update #2 - Extracted the getTip() method to an Interface called TipCalculator.  This guarantees that the method is available to all sub classes for their respective implementations.

---
+++ src/dip/lab2/student/solution1/TipCalculator.java
@@ -0,0 +1,12 @@
+package dip.lab2.student.solution1;
+
+/**
+ * Returns the tip for any type of service industry that requires tipping.
+ * 
+ * @author Dawn Bykowski
+ */
+public interface TipCalculator {
+
+    public abstract double getTip();
+    
+}
